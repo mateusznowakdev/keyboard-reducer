f00a421e2258ee83bafd34c39ba3cd7235603adc
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-04-20 19:48:58 +0200

    Import initial mockup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
 export default function App() {
-  return <h1>Vite + React</h1>;
+  return (
+    <>
+      <h1>Keyboard Reducer</h1>
+      <p>
+        Describe original and modified layouts here and check if you're missing anything.
+        <br />
+        Powered by React and Pyodide.
+      </p>
+      <p>
+        <a href="https://github.com/mateusznowakdev/keyboard-reducer">Source Code</a> &middot;{" "}
+        <a href="https://mateusznowak.dev">Back to Home Page</a>
+      </p>
+      <textarea>q w e r t y u i o p</textarea>
+      <div>
+        <strong>Original</strong> &middot; <a href="#">Modified</a> &middot; <a href="#">Labels</a>
+      </div>
+    </>
+  );
 }
