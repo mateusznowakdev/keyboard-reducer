69827423bf17d0e869fd00e0226dbefcbb1b1d97
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-07-13 21:37:22 +0200

    Download and unpack Pyodide automatically

---
+++ tools/pyodide-dl.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+PYODIDE_VERSION=0.23.1
+PYODIDE_PACKAGE=pyodide-core-${PYODIDE_VERSION}.tar.bz2
+PYODIDE_TARGET_DIR=pyodide/v${PYODIDE_VERSION}/full
+
+wget -nc https://github.com/pyodide/pyodide/releases/download/${PYODIDE_VERSION}/${PYODIDE_PACKAGE}
+
+rm -r pyodide || true
+tar xf ${PYODIDE_PACKAGE}
+mkdir -p ${PYODIDE_TARGET_DIR}
+find pyodide -maxdepth 1 -type f -exec mv {} ${PYODIDE_TARGET_DIR} \;
