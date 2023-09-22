34f7121818b00d64eb52382dc7e62745521f5212
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-09-22 13:34:17 +0200

    Move all static files to src directory

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
+import "./index.css";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
