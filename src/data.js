1d0e6fcf63ae3f075275a1cfe3bf45704a144f26
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2024-01-11 01:04:13 +0100

    Add more original buttons to the sample data

--- src/data.js
+++ src/data.js
@@ -1,13 +1,15 @@
-export const SAMPLE_ORIGINAL = `-- ! @ # $ % ^ & * ( ) _ + --
+export const SAMPLE_ORIGINAL = `--
+~ ! @ # $ % ^ & * ( ) _ +
 -- -- -- -- -- -- -- -- -- -- -- { } |
 -- -- -- -- -- -- -- -- -- -- : "
 -- -- -- -- -- -- -- -- -- < > ?
 
-esc 1 2 3 4 5 6 7 8 9 0 - = bksp
-tab q w e r t y u i o p [ ] \\
+esc -- f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 -- prscr scrlk pause
+\` 1 2 3 4 5 6 7 8 9 0 - = bksp -- ins home pgup
+tab q w e r t y u i o p [ ] \\ -- del end pgdn
 caps a,x s,x d,x f,x g,x h,x j,x k,x l,x ;,x ',x -- enter
-lshift,x -- z  x c v b n m , . / rshift,x
-lctrl lsuper lalt -- -- -- space,x -- -- -- -- ralt menu rctrl`;
+lshift,x -- z  x c v b n m , . / rshift,x -- -- -- up
+lctrl super lalt -- -- -- space,x -- -- -- -- ralt menu rctrl -- left down right`;
 
 export const SAMPLE_MODIFIED = `esc q w e r t y u i o p bksp
 tab a s d f g h j k l ! enter
@@ -16,4 +18,10 @@ lctrl z x c v space b n m , . ?
 -- 1 2 3 4 5 6 7 8 9 0 --`;
 
 export const SAMPLE_LABELS = `esc 🏃
-space `;
+space 
+left ←
+up ↑
+right →
+down ↓
+menu ≣
+super ❖`;
